2b400c42cacd995c1de5e8d0835f74eed313c089
Anton <freeman3x@mail.ru>
2022-06-04 10:55:45 +0300

    Добавил Дни недели

---
+++ Дни недели/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите номер дня недели: ");
+string username = Console.ReadLine();
+
+if(username == "1")
+{
+    Console.WriteLine("Понедельник");      
+}
+if(username == "2")
+{
+    Console.WriteLine("Вторник");      
+}
+if(username == "3")
+{
+    Console.WriteLine("Среда");      
+}
+if(username == "4")
+{
+    Console.WriteLine("Четверг");      
+}
+if(username == "5")
+{
+    Console.WriteLine("Пятница");      
+}
+if(username == "6")
+{
+    Console.WriteLine("Суботта");      
+}
+if(username == "7")
+{
+    Console.WriteLine("Воскресенье");      
+}
