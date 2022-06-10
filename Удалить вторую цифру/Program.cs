2d0f71d5e8348f70c83541573bfce851b8be4ba8
Anton <freeman3x@mail.ru>
2022-06-10 10:25:49 +0300

    Добавил Удалить вторую цифру

---
+++ Удалить вторую цифру/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите трехзначное число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int a = number / 100;
+
+int b = number % 100;
+
+int c = b / 10; 
+
+int d = number % 10; 
+
+Console.Write("первое число: ");
+Console.WriteLine(a);
+
+Console.Write("второе число: ");
+Console.WriteLine(c);
+
+Console.Write("третье число: ");
+Console.WriteLine(d);
+
+Console.Write("Новое число: ");
+Console.Write(a);Console.Write(d);
