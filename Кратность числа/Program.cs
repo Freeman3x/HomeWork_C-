eef3d8448cc538b1b00cbfdc6cdb0a89e53bbca0
Anton <freeman3x@mail.ru>
2022-06-10 10:49:48 +0300

    Добавил Кратность числа

---
+++ Кратность числа/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число: ");
+
+double number = double.Parse(Console.ReadLine());
+
+Console.Write("Введите делитель ");
+
+double a = double.Parse(Console.ReadLine());
+
+double b = number / a;
+
+double c = b % 1;
+
+if(c == 0)
+{
+    Console.WriteLine("Число кратно ");
+}
+else
+{
+    Console.Write("Число не кратно, остаток от деления = ");
+    Console.WriteLine(c);
+}
